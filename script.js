808c12f5435450416ecadf197ecfe0bc6f35080e
Zayne <zaynerahman@hotmail.com>
2026-03-14 23:29:11 +0000

    add vid to Python page

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function goToCourse(url) {
   window.open(url, "_blank");
 }
+function openVideo(videoId) {
+const modal = document.getElementById("videoModal");
+const frame = document.getElementById("videoFrame");
+frame.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+modal.style.display = "flex";
+}
+function closeVideo() {
+const modal = document.getElementById("videoModal");
+const frame = document.getElementById("videoFrame");
+frame.src = "";
+modal.style.display = "none";
+}
